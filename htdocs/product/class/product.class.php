3fad7e01a83e9abb94faae62a8a2413dc95300ce
Elisée Chemin <elisee.chemin@tetras-libre.fr>
2024-12-04 14:13:31 +0100

    Added a configuration option for the propagation to variants

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -1346,7 +1346,7 @@ class Product extends CommonObject
 				}
 
 				if (!$error) {
-					if (isModEnabled('variants')) {
+					if (isModEnabled('variants') && getDolGlobalString('PRODUIT_ATTRIBUTES_PROPAGATE', 1)) {
 						include_once DOL_DOCUMENT_ROOT.'/variants/class/ProductCombination.class.php';
 
 						$comb = new ProductCombination($this->db);
