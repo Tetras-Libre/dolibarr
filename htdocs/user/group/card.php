77be69870b0f429fa6410e9e3bf91ca19067b285
Elisée Chemin <elisee.chemin@tetras-libre.fr>
2025-04-04 16:53:14 +0200

    Removed checks for Multycompany that were redundant with code in the module

--- htdocs/user/group/card.php
+++ htdocs/user/group/card.php
@@ -75,11 +75,6 @@ $hookmanager->initHooks(array('groupcard', 'globalcard'));
 // Security check
 $result = restrictedArea($user, 'user', $id, 'usergroup&usergroup', $feature2);
 
-// Users/Groups management only in master entity if transverse mode
-if (isModEnabled('multicompany') && $conf->entity > 1 && $conf->global->MULTICOMPANY_TRANSVERSE_MODE) {
-	accessforbidden();
-}
-
 
 /**
  * Actions
