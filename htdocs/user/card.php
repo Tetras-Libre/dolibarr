8bc1c39da17c7f994a6e8071a3990adc048c772f
Elisée Chemin <elisee.chemin@tetras-libre.fr>
2025-04-23 17:27:47 +0200

    Moved the multicompany logic from user/card.php to a trigger

--- htdocs/user/card.php
+++ htdocs/user/card.php
@@ -328,21 +328,9 @@ if (empty($reshook)) {
 
 			// Set entity property
 			$entity = GETPOST('entity', 'int');
-			if (isModEnabled('multicompany')) {
-				if (GETPOST('superadmin', 'int')) {
-					$object->entity = 0;
-				} else {
-					if (getDolGlobalString('MULTICOMPANY_TRANSVERSE_MODE')) {
-						$object->entity = 1; // all users are forced into master entity
-					} else {
-						$object->entity = ($entity == '' ? 1 : $entity);
-					}
-				}
-			} else {
-				$object->entity = ($entity == '' ? 1 : $entity);
-				/*if ($user->admin && $user->entity == 0 && GETPOST("admin",'alpha'))
-				{
-				}*/
+			$object->entity = ($entity == '' ? 1 : $entity);
+			if (GETPOST('superadmin', 'int')) {
+				$object->entity = 0;
 			}
 
 			$db->begin();
