ea129cbd5e25d2dffc0bd9d9cad983a5622ed71f
Anthony Geourjon <anthony.geourjon@tetras-libre.fr>
2026-02-19 10:49:52 +0100

    Debug oidc in 22

--- htdocs/core/login/functions_openid_connect.php
+++ htdocs/core/login/functions_openid_connect.php
@@ -93,8 +93,12 @@ function check_user_password_openid_connect($usertotest, $passwordtotest, $entit
 		'client_id'     => getDolGlobalString('MAIN_AUTHENTICATION_OIDC_CLIENT_ID'),
 		'client_secret' => getDolGlobalString('MAIN_AUTHENTICATION_OIDC_CLIENT_SECRET'),
 		'code'          => $auth_code,
-		'redirect_uri'  => openid_connect_get_redirect_url()
+		'redirect_uri'  => getDolGlobalString('MAIN_AUTHENTICATION_OIDC_REDIRECT_URI')
 	];
+
+	dol_syslog("functions_openid_connect::check_user_password_openid_connect /old code=".print_r(openid_connect_get_redirect_url(), true), LOG_DEBUG);
+
+	dol_syslog("functions_openid_connect::check_user_password_openid_connect /authparam=".print_r($auth_param, true), LOG_DEBUG);
 
 	$token_response = getURLContent(getDolGlobalString('MAIN_AUTHENTICATION_OIDC_TOKEN_URL'), 'POST', http_build_query($auth_param), 1, array(), array('https'), 2);
 	$token_content = json_decode($token_response['content']);
