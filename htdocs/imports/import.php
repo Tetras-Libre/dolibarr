e40b0d1294cc1a0558970ce54927b18662a23835
Elisée Chemin <elisee.chemin@tetras-libre.fr>
2026-05-27 16:57:50 +0200

    Fixed a duplicate hook call (probably from a merge of ours)

--- htdocs/imports/import.php
+++ htdocs/imports/import.php
@@ -1955,13 +1955,6 @@ if ($step == 5 && $datatoimport) {
 						'lib' => implode("<br>", array_merge([$hookmanager->error], $hookmanager->errors))
 					];
 				}
-
-				$reshook = $hookmanager->executeHooks('AfterImportInsert', $parameters);
-				if ($reshook < 0) {
-					$arrayoferrors[$sourcelinenb][] = [
-						'lib' => implode("<br>", array_merge([$hookmanager->error], $hookmanager->errors))
-					];
-				}
 			}
 			// Close file
 			$obj->import_close_file();
